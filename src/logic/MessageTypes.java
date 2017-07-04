20e70fc375ed9a215368d68f48fd393829d2ed44
amir jalili <jalilia370@gmail.com>
2017-07-04 12:00:36 +0430

    initial commit

--- src/logic/MessageTypes.java
+++ src/logic/MessageTypes.java
@@ -3,7 +3,6 @@ package logic;
 /**
  * Created by amir on 7/3/17.
  */
-public class MessageTypes {
-    public static final byte PROTOCOL_VERSION = 1;
-    public static final byte REQUEST_LOGIN = 1;
+public enum MessageTypes {
+    LOGIN, CHAT;
 }
