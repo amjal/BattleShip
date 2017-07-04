1ec3c508f35505a7189d24b78f5383de582d8de2
parsaf <parsafarinnia@gmail.com>
2017-07-04 15:16:26 +0430

    1

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@
  * Created by amir on 6/28/17.
  */
 public class Main {
+
 }
--- src/view/SelectConnectionMode.java
+++ src/view/SelectConnectionMode.java
@@ -1,7 +1,22 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * Created by amir on 7/4/17.
  */
 public class SelectConnectionMode {
+
+    private JPanel panel1;
+    private JTextField textField1;
+    private JLabel name;
+    private JTextField textField2;
+    private JRadioButton hostRadioButton1;
+    private JRadioButton guestRadioButton;
+    private JTextField textField3;
+    private JTextField textField4;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
