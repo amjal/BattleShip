49a8538b1e58c54400a0f0dd59165fd22a51c11c
parsaf <parsafarinnia@gmail.com>
2017-07-05 18:33:33 +0430

    SelectConnectionMode completed! (amjal) gamePanel is roughly complete but problem with main gaming field(parsa) no gamepanel

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-import view.GamePanel;
-
 /**
  * Created by amir on 6/28/17.
  */
 public class Main {
+    /**
+     *    public void gamePanelMethod() {
+     gridPanel = new JPanel();
+     gridPanel.setLayout(new GridLayout(10, 10));
+     JLabel[][] cells = new JLabel[10][10];
+     for (int i = 0; i < 10; i++) {
+     for (int j = 0; j < 10; j++) {
+     cells[i][j] = new JLabel();
+     cells[i][j].setSize(50,50);
+     cells[i][j].setBackground(Color.RED);
+     cells[i][j].setBorder(new LineBorder(Color.black, 1));
+
+     cells[i][j].setVisible(true);
+     gridPanel.add(cells[i][j]);
+     }
+
+     }
+     cells[5][5].setForeground(Color.cyan);
+     gridPanel.setSize(500,500);
+     gamePlace.add(gridPanel);
+     }
+     * @param args
+     */
     public static void main(String[] args) {
-        new GamePanel();
+
 
 
     }
