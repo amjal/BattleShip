dbfe75564e32413e29669da703ae45f5633cb035
parsaf <parsafarinnia@gmail.com>
2017-07-06 11:48:48 +0430

    correct game panel
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,58 +9,60 @@ import java.awt.*;
  */
 public class Main {
     /**
-     *    public void gamePanelMethod() {
-     gridPanel = new JPanel();
-     gridPanel.setLayout(new GridLayout(10, 10));
-     JLabel[][] cells = new JLabel[10][10];
-     for (int i = 0; i < 10; i++) {
-     for (int j = 0; j < 10; j++) {
-     cells[i][j] = new JLabel();
-     cells[i][j].setSize(50,50);
-     cells[i][j].setBackground(Color.RED);
-     cells[i][j].setBorder(new LineBorder(Color.black, 1));
-
-     cells[i][j].setVisible(true);
-     gridPanel.add(cells[i][j]);
-     }
-
-     }
-     cells[5][5].setForeground(Color.cyan);
-     gridPanel.setSize(500,500);
-     gamePlace.add(gridPanel);
-     }
+     * public void gamePanelMethod() {
+     * gridPanel = new JPanel();
+     * gridPanel.setLayout(new GridLayout(10, 10));
+     * JLabel[][] cells = new JLabel[10][10];
+     * for (int i = 0; i < 10; i++) {
+     * for (int j = 0; j < 10; j++) {
+     * cells[i][j] = new JLabel();
+     * cells[i][j].setSize(50,50);
+     * cells[i][j].setBackground(Color.RED);
+     * cells[i][j].setBorder(new LineBorder(Color.black, 1));
+     * <p>
+     * cells[i][j].setVisible(true);
+     * gridPanel.add(cells[i][j]);
+     * }
+     * <p>
+     * }
+     * cells[5][5].setForeground(Color.cyan);
+     * gridPanel.setSize(500,500);
+     * gamePlace.add(gridPanel);
+     * }
+     * JPanel gridPanel = new JPanel();
+     * gridPanel.setLayout(new GridLayout(10, 10));
+     * JLabel[][] cells = new JLabel[10][10];
+     * for (int i = 0; i < 10; i++) {
+     * for (int j = 0; j < 10; j++) {
+     * cells[i][j] = new JLabel();
+     * cells[i][j].setSize(50,50);
+     * cells[i][j].setBackground(Color.RED);
+     * cells[i][j].setBorder(new LineBorder(Color.black, 1));
+     * <p>
+     * cells[i][j].setVisible(true);
+     * gridPanel.add(cells[i][j]);
+     * }
+     * <p>
+     * }
+     * cells[5][5].setForeground(Color.cyan);
+     * gridPanel.setSize(500,500);
+     * GamePanel gamePanel =new GamePanel(gridPanel);
+     * //gamePanel.setGamePlace(gridPanel);
+     *
      * @param args
      */
     public static void main(String[] args) {
 
-        JPanel gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(10, 10));
-        JLabel[][] cells = new JLabel[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                cells[i][j] = new JLabel();
-                cells[i][j].setSize(50,50);
-                cells[i][j].setBackground(Color.RED);
-                cells[i][j].setBorder(new LineBorder(Color.black, 1));
-
-                cells[i][j].setVisible(true);
-                gridPanel.add(cells[i][j]);
-            }
-
-        }
-        cells[5][5].setForeground(Color.cyan);
-        gridPanel.setSize(500,500);
-        GamePanel gamePanel =new GamePanel(gridPanel);
-        //gamePanel.setGamePlace(gridPanel);
-
-JFrame frame=new JFrame("test");
-frame.setSize(700,500);
-frame.setDefaultCloseOperation(3);
-//gamePanel.revalidate();
 
 
-frame.add(gamePanel.getFullPanel());
-frame.setVisible(true);
+        JFrame frame = new JFrame("test");
+        frame.setSize(700, 500);
+        frame.setDefaultCloseOperation(3);
+GamePanel gamePanel=new GamePanel();
+
+
+        frame.add(gamePanel.getFullPanel());
+        frame.setVisible(true);
 
     }
 }
